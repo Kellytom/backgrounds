6cd09cee4b3f4aefe728c72fa348fa60226d6fd5
Kellytom <kelly.aquinas.tom@gmail.com>
2025-07-03 02:57:57 +0000

    v1.0 - Working bottom navbar with custom dropdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,30 +4,19 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	devToolbar: {
+		enabled: false
+	},
 	integrations: [
 		starlight({
 			title: 'Hacker1',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [
-						{ label: 'Example Reference', slug: 'reference/example' },
-					],
-				},
-				{
-					label: 'Showcases',
+					label: 'Main',
 					items: [
 						{ label: 'Starlight Showcase', slug: 'starlight-showcase' },
 						{ label: 'DaisyUI Showcase', slug: 'daisyui-showcase' },
-						{ label: 'CSS Guide', slug: 'starlight-css-guide' },
 					],
 				},
 			],
